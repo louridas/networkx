c3bac6a6bda6ffefb0e0091bb9d31cebc9a49565
Thodoris Sotiropoulos <theosotr@windowslive.com>
2015-06-13 21:25:38 +0300

    Enhance code readability

--- networkx/algorithms/communities.py
+++ networkx/algorithms/communities.py
@@ -13,7 +13,7 @@ __author__ = """\n""".join(['Konstantinos Karakatsanis '
 __all__ = ['louvain']
 
 
-def louvain(G):
+def louvain(G, weight='weight'):
     """Find communities in graph using the Louvain method.
 
     Parameters
@@ -25,18 +25,6 @@ def louvain(G):
     A dictionary which keys are the nodes of the Graph and its values are the
     communities that they belong
 
-    Examples
-    --------
-    >>> G = nx.erdos_renyi_graph(50, 0.01)
-    >>> comp = louvain(G)
-    >>> comp[0]
-    {0: 48, 1: 48, 2: 42, 3: 42, 4: 42, 5: 11, 6: 33, 7: 33, 8: 33, 9: 23,
-    10: 34, 11: 11, 12: 21, 13: 11, 14: 21, 15: 35, 16: 35, 17: 35, 18: 37,
-    19: 37, 20: 24, 21: 21, 22: 21, 23: 23, 24: 24, 25: 24, 26: 24, 27: 48,
-    28: 48, 29: 48, 30: 11, 31: 43, 32: 42, 33: 33, 34: 34, 35: 35, 36: 35,
-    37: 37, 38: 21, 39: 21, 40: 21, 41: 21, 42: 42, 43: 43, 44: 43, 45: 11,
-    46: 11, 47: 11, 48: 48, 49: 48}
-
     Notes
     -----
     The Louvain Method for community detection is a method to extract
@@ -45,24 +33,24 @@ def louvain(G):
     """
     H = G.copy()
     tree = []
-    if H.size(weight='weight') == 0:
+
+    if G.number_of_edges() == 0:
         msg = 'The graph has undefined modularity.'
         raise nx.NetworkXError(msg)
-    elif H.is_directed():
+
+    if H.is_directed():
         msg = 'The graph has undefined modularity.'
         raise nx.NetworkXError(msg)
+
     improve = True
     while improve:
-        p = _one_pass(H)
-        try:
-            tree.append(p)
-        except NameError:
-            tree = [p]
-        improve, H = _partition_to_graph(H, p)
+        p = _one_pass(H, weight)
+        tree.append(p)
+        improve, H = _partition_to_graph(H, p, weight)
     return tree
 
 
-def _one_pass(G):
+def _one_pass(G, weight):
     """
     Puts the Graph nodes into communities as long as it has effect to the
     modularity.
@@ -73,37 +61,47 @@ def _one_pass(G):
     :return: The graph given separated into communities
     """
     increase = True
-    p = {}
-    inner = {}
-    tot = {}
-    for u in G:
-        p[u] = u
-        inner[u], tot[u] = _init(G, u)
+    m = G.size(weight=weight)
+
+    # Initialize p, inner and tot.
+    p = {u: u for u in G}
+    inner = {u: 0 if not G.has_edge(u, u)
+             else G.get_edge_data(u, u).get(weight, 1) for u in G}
+    tot = {u: G.degree(u, weight=weight) for u in G}
+
+    # Auxiliary functions.
+    k_in = lambda i, com: sum(G.get_edge_data(i, j).get(weight, 1)
+                              for j in G.neighbors(i) if p[j] == com)
+    gain = lambda n, com: float(k_in(n, com)) / (2 * m) - float(
+                tot[com] * G.degree(n, weight=weight)) / (2 * pow(m, 2))
+
     while increase:
         increase = False
         for u in G:
             c_old = p[u]
-            p, inner[u], tot[u] = _remove(G, u, c_old, p, inner[u], tot[u])
-            if G.neighbors(u) != 0:
-                max_gain = 0
-                best = None
-                for v in nx.neighbors(G, u):
-                    if u != v:
-                        c = p[v]
-                        if _gain(G, u, c, p, tot[c]) > max_gain:
-                            max_gain = _gain(G, u, c, p, tot[c])
-                            best = v
-            if best is None:
-                c_new = c_old
-            else:
-                c_new = p[best]
-            p, inner[u], tot[u] = _insert(G, u, c_new, p, inner[u], tot[u])
-            if c_old is not c_new:
+            inner[u] -= k_in(u, c_old) if not G.has_edge(u, u) \
+                else (k_in(u, c_old)) + G.get_edge_data(u, u).get(weight, 1)
+            tot[u] -= G.degree(u, weight=weight)
+            max_gain = 0
+            best = None
+            for v in G.neighbors(u):
+                if u != v:
+                    c = p[v]
+                    diff = gain(u, c)
+                    if diff > max_gain:
+                        max_gain = diff
+                        best = v
+            c_new = c_old if best is None else p[best]
+            inner[u] += k_in(u, c_new) if not G.has_edge(u, u) \
+                else (k_in(u, c_new)) + G.get_edge_data(u, u).get(weight, 1)
+            tot[u] += G.degree(u, weight=weight)
+            p[u] = c_new
+            if c_old != c_new:
                 increase = True
     return p
 
 
-def _partition_to_graph(G, p):
+def _partition_to_graph(G, p, weight):
     """
     Creates a Graph that its nodes represent communities and its edges
     represent the connections between these communities.
@@ -118,106 +116,12 @@ def _partition_to_graph(G, p):
     G2 = nx.Graph()
     G2.add_nodes_from(p.values())
     for u, v, data in G.edges(data=True):
-        u_v_weight = data.get("weight", 1)
-        weight = G2.get_edge_data(p[u], p[v], {'weight': 0}).get('weight', 1) \
-                 + u_v_weight
-        G2.add_edge(p[u], p[v], weight=weight)
-    em = iso.numerical_edge_match('weight', 1)
+        u_v_weight = data.get(weight, 1)
+        edge_weight = u_v_weight if not G2.has_edge(p[u], p[v])\
+            else G2.get_edge_data(p[u], p[v]).get(weight, 1) + u_v_weight
+        if G2.has_edge(p[u], p[v]):
+            G2.edge[p[u]][p[v]][weight] = edge_weight
+        else:
+            G2.add_edge(p[u], p[v], weight=edge_weight)
+    em = iso.numerical_edge_match(weight, 1)
     return (not nx.is_isomorphic(G, G2, edge_match=em)), G2
-
-
-def _init(G, u):
-    """
-    Updates the inner and tot parameters.
-
-    It is part of Louvain's algorithm.
-
-    :param G: NetworkX graph
-    :param u: A node of the Graph
-    :return: Updated inner and tot parameters
-    """
-    inner = G.get_edge_data(u, u, {'weight': 0}).get('weight', 1)
-    tot = G.degree(u)
-    return inner, tot
-
-
-def _remove(G, u, c, p, inner, tot):
-    """
-    Calculates the inner and tot parameters after extracting the node u from
-    the community c.
-
-    It is part of Louvain's algorithm.
-
-    :param G: NetworkX graph
-    :param u: A node of the Graph
-    :param c: The community the node u was before
-    :param p: The current partition of the nodes
-    :param inner: Sum of all the weights of the links inside the community that
-        the node u was moved into
-    :param tot: Sum of all the weights of the links to nodes in the community
-    :return: Updated p, inner and tot parameters
-    """
-    inner -= _k_in(G, u, c, p) + \
-             G.get_edge_data(u, u, {'weight': 0}).get('weight', 1)
-    tot -= G.degree(u)
-    p[u] = []
-    return p, inner, tot
-
-
-def _insert(G, u, c, p, inner, tot):
-    """
-    Calculates the inner and tot parameters after inserting the node u into
-    the community c.
-
-    It is part of Louvain's algorithm.
-
-    :param G: NetworkX graph
-    :param u: A node of the Graph
-    :param c: The community the node u is moving into
-    :param p: The current partition of the nodes
-    :param inner: Sum of all the weights of the links inside the community that
-        the node u is moving into
-    :param tot: Sum of all the weights of the links to nodes in the community
-    :return: Updated p, inner and tot parameters
-    """
-    inner += _k_in(G, u, c, p) + \
-             G.get_edge_data(u, u, {'weight': 0}).get('weight', 1)
-    tot += G.degree(u)
-    p[u] = c
-    return p, inner, tot
-
-
-def _gain(G, u, c, p, tot):
-    """
-    Calculates the change in the modularity.
-
-    It is part of Louvain's algorithm.
-
-    :param G: NetworkX graph
-    :param u: A node of the Graph
-    :param c: The community the node u is going to move into
-    :param p: The current partition of the nodes
-    :param tot: Sum of all the weights of the links to nodes in the community
-    :return: The change in modularity
-    """
-    m = G.size(weight='weight')
-    return float(_k_in(G, u, c, p)) / (2 * m) - \
-           float(tot * G.degree(u)) / (2 * pow(m, 2))
-
-
-def _k_in(G, u, c, p):
-    """
-    Calculates the sum of the weights of the links between node u and other
-    nodes in the community.
-
-    It is part of Louvain's algorithm.
-
-    :param G: NetworkX graph
-    :param u: A node of the Graph
-    :param c: The community the node u is moving into
-    :param p: The current partition of the nodes
-    :return: Sum of the weights of the links between node u and other nodes
-        in the community
-    """
-    return sum(G.get_edge_data(u, v, {'weight': 0}).get('weight', 1)
-               for v in G if p[v] == c)
